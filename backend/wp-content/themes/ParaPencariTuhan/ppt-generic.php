808c99996c5a3d005376a5daebd65ce8a6ba3d99
enoa7 <gamal.ardi@gmail.com>
2016-03-01 14:02:48 +0700

    add the generic template

---
+++ backend/wp-content/themes/ParaPencariTuhan/ppt-generic.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: ppt-generic
+ *
+ * @package ParaPencariTuhan
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php the_post_thumbnail(); ?>
+
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+<?php get_footer(); ?>
