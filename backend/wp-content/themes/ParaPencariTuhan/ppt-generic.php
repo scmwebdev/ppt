43d16c09152bef01ec1537209989657c5eb60656
enoa7 <gamal.ardi@gmail.com>
2016-03-01 18:02:12 +0700

    add partial-testimonial template for homepage

--- backend/wp-content/themes/ParaPencariTuhan/ppt-generic.php
+++ backend/wp-content/themes/ParaPencariTuhan/ppt-generic.php
@@ -9,8 +9,30 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<?php //the_post_thumbnail('mainBanner', array( 'class' => 'img-responsive __fullwidth')); ?>
-		<?php responsiveBanner(); ?>
+		<div class="mainbanner">
+			<?php responsiveBanner(); ?>	
+		</div>
+		<?php 
+			if ( is_front_page() ) { 
+				// set up or arguments for our custom query
+				$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+				$query_args = array(
+					'post_type' => 'post',
+					'category_name' => 'testimonial'
+					'posts_per_page' => 5,
+					'paged' => $paged
+				);
+				// create a new instance of WP_Query
+				$the_query = new WP_Query( $query_args );
+			?>
+			<div class="testimonial">
+				<div class="container">
+					<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();  ?>
+			  			<?php get_template_part( 'template-parts/partial', 'testimonial' ); ?>
+					<?php endwhile; ?>
+				</div>
+			</div>
+			<?php } ?>
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
 <?php get_footer(); ?>
---
+++ backend/wp-content/themes/ParaPencariTuhan/template-parts/partial-testimonial.php
@@ -0,0 +1,3 @@
+<div class="item-list">
+	<div class="item-list-desc"><?php the_content(); ?></div>
+</div>
