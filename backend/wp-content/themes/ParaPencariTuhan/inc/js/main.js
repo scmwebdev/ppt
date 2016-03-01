9e5720e2a4c2314c5f63994408133b4663ac4874
enoa7 <gamal.ardi@gmail.com>
2016-03-01 17:37:38 +0700

    add animatecss

--- backend/wp-content/themes/ParaPencariTuhan/inc/js/main.js
+++ backend/wp-content/themes/ParaPencariTuhan/inc/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
 	console.log('start custom js');
+	var siteTitle = $('.site-title');
+	siteTitle.addClass('animated bounceInLeft');
+	setInterval(function () {
+        siteTitle.removeClass('bounceInLeft');
+    },2000);
+	
 });
