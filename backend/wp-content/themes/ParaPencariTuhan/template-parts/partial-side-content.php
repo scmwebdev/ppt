37e36bbac2232d08fbdc08f93c37fe4735d47958
enoa7 <gamal.ardi@gmail.com>
2016-03-03 17:30:27 +0700

    embed twitter timeline

--- backend/wp-content/themes/ParaPencariTuhan/template-parts/partial-side-content.php
+++ backend/wp-content/themes/ParaPencariTuhan/template-parts/partial-side-content.php
@@ -16,3 +16,6 @@
 		
 <?php endif; ?>
 <?php wp_reset_postdata(); ?>
+
+<a class="twitter-timeline" href="https://twitter.com/hashtag/TheDanceIcon2" data-widget-id="705335261517885440">#TheDanceIcon2 Tweets</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
