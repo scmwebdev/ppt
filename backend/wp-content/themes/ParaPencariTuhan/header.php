dd5db0eb494d563c4b9ce15963c26083f7e5cba3
gamal <gamal.pratama@scm.co.id>
2016-03-23 12:58:26 +0700

    add GA

--- backend/wp-content/themes/ParaPencariTuhan/header.php
+++ backend/wp-content/themes/ParaPencariTuhan/header.php
@@ -20,11 +20,24 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	
 	<?php endif; ?>
+
 	<?php wp_head(); ?>
+
+	<!-- GA -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-45403057-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
-
 <!-- Facebook SDK -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -34,6 +47,7 @@
   js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.5";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
 
 <div id="page" class="site">
 	<div class="site-inner">
